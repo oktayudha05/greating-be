da0bc1315034ecc31044423715d4f96f2147585d
Oktario Mufti Yudha <oktayudha05@gmail.com>
2026-03-04 01:29:28 +0000

    setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Halo boss ready parah nih awak')
+})
+
+app.get('/health', (req, res) => {
+  res.send('awak sehat brayy')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
